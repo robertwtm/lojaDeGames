c93df2ec2377c0cd3da0d3215c969d90722dea21
giovana borges <gigi12345as17@gmail.com>
2021-12-07 14:58:53 -0300

    change categoriaRepository

--- src/main/java/org/generation/lojaDeGames/repository/CategoriaRepository.java
+++ src/main/java/org/generation/lojaDeGames/repository/CategoriaRepository.java
@@ -3,9 +3,7 @@ package org.generation.lojaDeGames.repository;
 import org.generation.lojaDeGames.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
-
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
