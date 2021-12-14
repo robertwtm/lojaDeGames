79babfd3f9e9777bca1fdac128265c36218af9c5
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-14 13:07:49 -0300

    Update Produto.java

--- src/main/java/org/generation/lojaDeGames/model/Produto.java
+++ src/main/java/org/generation/lojaDeGames/model/Produto.java
@@ -1,14 +1,10 @@
 package org.generation.lojaDeGames.model;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
