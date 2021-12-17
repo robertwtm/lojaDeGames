2b391f2bb496ac0955f4c9169e3c1ef804f02a47
Robert Willian Tavares Mendes <31051742+robertwtm@users.noreply.github.com>
2021-12-17 19:30:28 -0300

    Update UsuarioRepository.java

--- src/main/java/org/generation/lojaDeGames/repository/UsuarioRepository.java
+++ src/main/java/org/generation/lojaDeGames/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package org.generation.lojaDeGames.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.generation.lojaDeGames.model.Usuario;
@@ -7,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 	public Optional<Usuario> findByUsuario(String usuario);
+	public List<Usuario> findAllByNomeContainingIgnoreCase(String nome);
 }
